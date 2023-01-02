b0c0729790c7656629e781c6f52e3247afa9a768
neeti-nk47 <121576070+neeti-nk47@users.noreply.github.com>
2023-01-03 01:29:38 +0530

    scalable

--- index.js
+++ index.js
@@ -10,12 +10,24 @@ function addItem(e){
     var inphone= document.getElementById('phone').value;
     var indate= document.getElementById('date').value;
     var intime = document.getElementById('time').value;
+    
+    let userDetails = {
+        Name : inname,
+        Email : inmail,
+        Mobile : inphone,
+        CallDate: indate,
+        CallTime: intime
+    };
 
-    localStorage.setItem("name", inname);
-    localStorage.setItem("mail", inmail);
-    localStorage.setItem("phone", inphone);
-    localStorage.setItem("date", indate);
-    localStorage.setItem("time", intime);
+    let SerialUserDetail = JSON.stringify(userDetails);
+    localStorage.setItem(userDetails.Name, SerialUserDetail);
+    
+    //let reverse= JSON.parse(localStorage.getItem("Details"));
+    // localStorage.setItem("name", inname);
+    // localStorage.setItem("mail", inmail);
+    // localStorage.setItem("phone", inphone);
+    // localStorage.setItem("date", indate);
+    // localStorage.setItem("time", intime);
 
 }
 
