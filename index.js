c7611ea2e26e3e9937fa01ea9399c544c5a3e41f
neeti-nk47 <121576070+neeti-nk47@users.noreply.github.com>
2023-01-03 00:33:08 +0530

    major changes

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-function getcall(event){
-    event.preventDefault();
-    a=event.target.name.value
-    b=event.target.mail.value
-    c=event.target.phone.value
-    console.log(a,b,c)
+var form = document.getElementById('addForm');
+
+form.addEventListener('submit', addItem);
+
+function addItem(e){
+    e.preventDefault(); 
+
+    var inname = document.getElementById('name').value;
+    var inmail = document.getElementById('mail').value;
+    var inphone= document.getElementById('phone').value;
+    var indate= document.getElementById('date').value;
+    var intime = document.getElementById('time').value;
+
+    localStorage.setItem("name", inname);
+    localStorage.setItem("mail", inmail);
+    localStorage.setItem("phone", inphone);
+    localStorage.setItem("date", indate);
+    localStorage.setItem("time", intime);
+
 }
+
