e75d28229fe60cfe0f961b286bd4c920654e66f2
Neeti Kumar <121576070+neeti-nk47@users.noreply.github.com>
2023-01-18 21:32:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function addItem(){  
-    const itemList = document.getElementById('items'); 
-    
-    const name = document.getElementById('name');
-    const mail = document.getElementById('mail');
-    const phone= document.getElementById('phone');
-    const dated= document.getElementById('date');
-    const timed = document.getElementById('time');
+const itemList = document.getElementById('items'); 
 
-    let inname = name.value;
+const names = document.getElementById('name');
+const mail = document.getElementById('mail');
+const phone= document.getElementById('phone');
+const dated= document.getElementById('date');
+const timed = document.getElementById('time');
+
+function addItem(){    
+    let inname = names.value;
     let inmail = mail.value;
     let inphone = phone.value;
     let indate = dated.value;
@@ -20,24 +20,25 @@ function addItem(){
         CallDate: indate,
         CallTime: intime
     };
-    let SerialUserDetail = JSON.stringify(userDetails);
 
-    //new list item   
-
-    let li = document.createElement('li');
-    li.className = 'list-group-item';
-    li.innerText = inmail + " : " +SerialUserDetail ;
-    axios.post("https://crudcrud.com/api/c02fe1eb544d446fb2b5d346365a5a99/AppointmentData/",userDetails)  
+    axios.post("https://crudcrud.com/api/ceca46bb233145059afc4bd140f54654/AppointmentData/",userDetails)  
         .then(res => console.log(res))
         .catch(err => console.log(err));
-    //localStorage.setItem(inmail, SerialUserDetail);
+
+    addToScreen(userDetails);    
+}
+
+function addToScreen(obj){
+    //new list item      
+    let li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.innerText = `${obj.Name},${obj.Email},${obj.Mobile},${obj.CallDate},${obj.CallTime}`;       
 
     //Del button
     let deleteButton = document.createElement("button");
     deleteButton.innerText = "Delete";
-    deleteButton.className = 'btn btn-danger float-right delete';
+    deleteButton.className = 'btn btn-danger float-right';
     deleteButton.onclick = function() {
-        localStorage.removeItem(inmail); 
         itemList.removeChild(li);
     };
     li.appendChild(deleteButton);
@@ -45,15 +46,14 @@ function addItem(){
     //Edit buttom
     let editButton = document.createElement("button");
     editButton.innerText = "Edit";
-    editButton.className = 'btn btn-primary float-right edit';
+    editButton.className = 'btn btn-primary float-right';
     editButton.onclick = function() {
-        localStorage.removeItem(inmail) 
-
-        name.value = inname;
-        mail.value = inmail;
-        phone.value = inphone;  
-        dated.value = indate;
-        timed.value = intime;      
+        
+        names.value = obj.Name;
+        mail.value = obj.Email;
+        phone.value = obj.Mobile;  
+        dated.value = obj.CallDate;
+        timed.value = obj.CallTime;      
         
         itemList.removeChild(li);
     };    
@@ -62,7 +62,17 @@ function addItem(){
     itemList.appendChild(li);
 }
 
+window.addEventListener('DOMContentLoaded', (event) => {
+    axios.get("https://crudcrud.com/api/ceca46bb233145059afc4bd140f54654/AppointmentData/")
+    .then(res => {
+        console.log(res)
 
+        for (let i=0; i<=res.data.length; i++){
+            addToScreen(res.data[i])                        
+        }
+    })
+    .catch(err => console.log(err))    
+})
     
 
 
